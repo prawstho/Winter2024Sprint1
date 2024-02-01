681c40dd4192c15c644f7fd1e21da3159f2bd702
Peter <113168584+prawstho@users.noreply.github.com>
2024-02-01 10:15:56 -0330

    app init feature complete

--- myapp.js
+++ myapp.js
@@ -2,6 +2,8 @@ global.DEBUG = true;
 
 const fs = require("fs");
 
+const { initializeApp } = require('./init.js');
+
 const myArgs = process.argv.slice(2);
 
 if(DEBUG) if(myArgs.length >= 1) console.log('the myapp.args: ', myArgs);
@@ -10,6 +12,7 @@ switch (myArgs[0]) {
   case 'init':
   case 'i':
       if(DEBUG) console.log(myArgs[0], ' - initialize the app.');
+      initializeApp();
       break;
   case 'config':
   case 'c':
