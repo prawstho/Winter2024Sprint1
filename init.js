d5e887024e18bbcf270688d403985a39c2904148
Peter <113168584+prawstho@users.noreply.github.com>
2024-02-06 14:12:41 -0330

    implemented config an templates

--- init.js
+++ init.js
@@ -1,27 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const fsPromises = require('fs').promises;
-
-const folders = ['models', 'views', 'routes', 'logs', 'json'];
-
-const configjson = { 
-  name: 'AppConfigCLI',
-  version: '1.0.0',
-  description: 'The Command Line Interface (CLI) for the MyApp.',
-  main: 'myapp.js',
-  superuser: 'adm1n',
-  database: 'exampledb'
-};
-
-const tokenjson = [{
-  created: '1969-01-31 12:30:00',
-  username: 'username',
-  email: 'user@example.com',
-  phone: '5556597890',
-  token: 'token',
-  expires: '1969-02-03 12:30:00',
-  confirmed: 'tbd'
-}];
+const { folders, configjson, tokenjson } = require('./templates');
 
 function createFolders() {
   if(DEBUG) console.log('init.createFolders()');
